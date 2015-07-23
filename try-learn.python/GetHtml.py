7d7ffe522b46f7678a068fd3b31e93eef261c1f3
wuhuanhost <post.thinking@gmail.com>
2015-07-23 10:35:09 +0800

    update......

--- try-learn.python/GetHtml.py
+++ try-learn.python/GetHtml.py
@@ -4,14 +4,15 @@ import sys
 reload(sys) 
 sys.setdefaultencoding( "utf-8" ) 
 
-##请求网页
+#请求网页
 r = requests.get('http://www.zhidaow.com')
-##为请求到的页面进行编码
+#为请求到的页面进行编码
 r.encoding='utf-8'#网页编码成utf-8
-##输出请求的内容
+#输出请求的内容
 # print r.text
 
 
+##通过淘宝ip接口获取ip信息的方法
 def queryIP(ipstr):
     ip = requests.get('http://ip.taobao.com/service/getIpInfo.php?ip='+ipstr)
     print ip.json()['data']['country']
