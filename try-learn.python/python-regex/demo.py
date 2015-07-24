26c1fcae88b592d0f09a30c243772dd49e140d20
wuhuanhost <post.thinking@gmail.com>
2015-07-24 19:14:19 +0800

    add demo.rb and demo.py

---
+++ try-learn.python/python-regex/demo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import re
+
+line="qwepythonsdsadasapython"
+
+m1=re.search(r'python', line)
+m2=re.match(r'python',line)
+m3=re.findall(r'python',line)
+
+print m1.group()#匹配到python
+#print m2.group()#匹配不到，报错
+print m3 #匹配到两个
+
+#【search】、【match】、【findall】的区别
+## match要求字符串的第一个字符就能匹配上正则表达式，即从字符串的开头开始匹配，匹配到一个就停止
+## search从任意位置开始匹配，匹配到一个就停止
+## findall匹配所有的符合条件的字符串,返回一个数组
---
+++ try-learn.ruby/ruby-regex/demo.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+# demo1
+str="dasdpythondasdsPython"
+puts str.scan(/[pP]ython/)#查询python和Python
+puts str.sub(/[pP]ython/,"@@@")#替换第一次匹配到的python为@@@
+puts str.gsub(/[pP]ython/,"@@@")#查询python和Python并且将它们替换为@@@
+##【scan】查找方法
+##【gsub!】查找并且替换
+##【gsub/sub】会返回一个新的字符串
+##【gsub!/sub!】会修改被调用的字符串
+
+
+#demo2
+str1='<img src="test1.jpg" width="60px" height="80px"/><img src="test2.jpg" width="60px" height="80px"/>'
+puts str1.scan(/src=".*"/)#匹配【src="test1.jpg" width="60px" height="80px"/><img src="test2.jpg" width="60px" height="80px"】
+puts str1.scan(/src=".*?"/)#匹配图片的src属性及其值【？】是惰性匹配，匹配到就结束
+#【.*】表示匹配任意的非换行字符0到多次
+
+
+#demo3
+puts "[ qwe  dasd ]".gsub!(/\[([^\[\]]*?)\]/) {|s| s.gsub(/ /,"@@@")}
+#替换中括号中的空格为@
