1235f02e617c5d645c39b9bc38d579ac56b613a7
wuhuanhost <post.thinking@gmail.com>
2015-07-24 19:30:09 +0800

    update tips.rb

--- try-learn.ruby/tips.rb
+++ try-learn.ruby/tips.rb
@@ -1,7 +1,10 @@
 # encoding: utf-8
+
 #tips1 交换两个数的大小
-aa=1;
-bb=2
-puts "交换前： a=>#{aa}  b=>#{bb}"
-aa,bb=bb,aa;#交换aa和bb的值
-puts "交换后： a=>#{aa}  b=>#{bb}"
+def exchangeVariable(x,y)
+	puts "交换前： x=>#{x}  x=>#{y}"
+	x,y=y,x
+	puts "交换后： x=>#{x}  y=>#{y}"
+end
+
+exchangeVariable(1,2);
