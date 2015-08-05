fd38ecbffd28c0a8ddc4ee2ff1c8b7e5a4bb52d6
wuhuanhost <post.thinking@gmail.com>
2015-08-05 14:19:43 +0800

    update color.html

--- try-learn.python/pinyin.py
+++ try-learn.python/pinyin.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
-from pypinyin import pinyin, lazy_pinyin
-from pypinyin import lazy_pinyin
-
+from pypinyin import pinyin, lazy_pinyin, TONE2
+from snownlp import SnowNLP
 import pypinyin
+
+#项目地址https://github.com/mozillazg/python-pinyin
 
 print pinyin(u'中心', style=pypinyin.TONE2, heteronym=True)
 
 print lazy_pinyin(u'中心',style=pypinyin.TONE2)#加入了分词处理
 
+print "--------------------------我是分割线---------------------------------"
 
-#项目地址https://github.com/mozillazg/python-pinyin
+hans = u'单于简单123作者着急'
+hans_seg = SnowNLP(hans).words  # 分词处理
+hans_seg
+rege=lazy_pinyin(hans_seg, style=TONE2)
+for sss in rege:
+   print sss
 
