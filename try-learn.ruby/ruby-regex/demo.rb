a2eeadad311cbf9ef565ef2b38f029ff095a9691
wuhuanhost <post.thinking@gmail.com>
2015-08-10 15:13:29 +0800

    update demo.rb

--- try-learn.ruby/ruby-regex/demo.rb
+++ try-learn.ruby/ruby-regex/demo.rb
@@ -10,14 +10,18 @@ puts str.gsub(/[pP]ython/,"@@@")#查询python和Python并且将它们替换为@@
 ##【gsub/sub】会返回一个新的字符串
 ##【gsub!/sub!】会修改被调用的字符串
 
-
 #demo2
 str1='<img src="test1.jpg" width="60px" height="80px"/><img src="test2.jpg" width="60px" height="80px"/>'
 puts str1.scan(/src=".*"/)#匹配【src="test1.jpg" width="60px" height="80px"/><img src="test2.jpg" width="60px" height="80px"】
 puts str1.scan(/src=".*?"/)#匹配图片的src属性及其值【？】是惰性匹配，匹配到就结束
 #【.*】表示匹配任意的非换行字符0到多次
 
-
 #demo3
 puts "[ qwe  dasd ]".gsub!(/\[([^\[\]]*?)\]/) {|s| s.gsub(/ /,"@@@")}
 #替换中括号中的空格为@
+
+#demo4
+puts "-----------------------------------------------------------------"
+test="<u>　　　　　　</u>"
+puts  test.scan(/<u>[　]{0,200}<\/u>/)#匹配<u>和</u>标签中带有0，200个空格
+
