5221e97b3de49a905d21899ed44fed88ba250690
wuhuanhost <post.thinking@gmail.com>
2015-09-08 17:29:53 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-###练习demo
+
+###sublime text3常用插件列表
+1. Emmet【快速写html、css】
+2. MarkdownPreview【markdown】
+3. ColorPicker【调色板】
+4. AllAutocomplete【增强代码提示】
+5. DocBlockr【写注释的工具】
+
+
+###sublime text3配置文件
+
+```
+{
+	"color_scheme": "Packages/User/SublimeLinter/Monokai (SL).tmTheme",
+	"font_face": "Monaco",
+	"font_size": 10,
+	"gutter": true,
+	"highlight_line": true,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"line_numbers": true,
+	"tab_size": 4,
+	"word_warp": "auto",
+	"wrap_width": 0
+}
+```
+
+###sublime text3快捷键
+
+
+
+
+
+
+
 [wiki](https://github.com/wuhuanhost/mydemo/wiki)
--- try-learn.ruby/ruby-regex/demo.rb
+++ try-learn.ruby/ruby-regex/demo.rb
@@ -2,9 +2,9 @@
 
 # demo1
 str="dasdpythondasdsPython"
-puts str.scan(/[pP]ython/)#查询python和Python
-puts str.sub(/[pP]ython/,"@@@")#替换第一次匹配到的python为@@@
-puts str.gsub(/[pP]ython/,"@@@")#查询python和Python并且将它们替换为@@@
+###puts str.scan(/[pP]ython/)#查询python和Python
+###puts str.sub(/[pP]ython/,"@@@")#替换第一次匹配到的python为@@@
+###puts str.gsub(/[pP]ython/,"@@@")#查询python和Python并且将它们替换为@@@
 ##【scan】查找方法
 ##【gsub!】查找并且替换
 ##【gsub/sub】会返回一个新的字符串
@@ -12,30 +12,41 @@ puts str.gsub(/[pP]ython/,"@@@")#查询python和Python并且将它们替换为@@
 
 #demo2
 str1='<img src="test1.jpg" width="60px" height="80px"/><img src="test2.jpg" width="60px" height="80px"/>'
-puts str1.scan(/src=".*"/)#匹配【src="test1.jpg" width="60px" height="80px"/><img src="test2.jpg" width="60px" height="80px"】
-puts str1.scan(/src=".*?"/)#匹配图片的src属性及其值【？】是惰性匹配，匹配到就结束
+###puts str1.scan(/src=".*"/)#匹配【src="test1.jpg" width="60px" height="80px"/><img src="test2.jpg" width="60px" height="80px"】
+###puts str1.scan(/src=".*?"/)#匹配图片的src属性及其值【？】是惰性匹配，匹配到就结束
 #【.*】表示匹配任意的非换行字符0到多次
 
 #demo3
-puts "[ qwe  dasd ]".gsub!(/\[([^\[\]]*?)\]/) {|s| s.gsub(/ /,"@@@")}
+###puts "[ qwe  dasd ]".gsub!(/\[([^\[\]]*?)\]/) {|s| s.gsub(/ /,"@@@")}
 #替换中括号中的空格为@
 
 #demo4
-puts "-----------------------------demo4------------------------------------"
+###puts "-----------------------------demo4------------------------------------"
 test="<u>　　　　　　</u>"
-puts  test.scan(/<u>[　]{0,200}<\/u>/)#匹配<u>和</u>标签中带有0，200个空格
+###puts  test.scan(/<u>[　]{0,200}<\/u>/)#匹配<u>和</u>标签中带有0，200个空格
 
 
 
 #demo5
-puts "-----------------------------demo5------------------------------------"
+###puts "-----------------------------demo5------------------------------------"
 test1="qwwe<u><img src='img/10_zzxx.003.jpeg' style='border-bottom:1px solid #000000;padding-bottom:10px;'/></u>eqweqweqw";
-puts test1.scan(/<u><img.*?\/><\/u>/);
+###puts test1.scan(/<u><img.*?\/><\/u>/);
 
 
 #demo6
-puts "-----------------------------demo6------------------------------------"
+###puts "-----------------------------demo6------------------------------------"
 test2="dadsad123_-*&^%.png";
-puts test2.scan(/.*[(\.jpg)|(\.jpeg)|(\.png)]/);##按照文件名后缀进行查找
+###puts test2.scan(/.*[(\.jpg)|(\.jpeg)|(\.png)]/);##按照文件名后缀进行查找
+
+
+
+
+
+puts "-------------------------------------------------------"
+test123="img/abcd.jpg{200,300}   img/dasdsa.jpg{400,300}img/dakdkaj.png{600,100}   img/dadadsda.gif   fjksdhfkshdfjkhjkfhkjdhfkjlhkjlh";
+a= test123.scan(/(img\/[^\.]+?.(jpg|jpeg|png|gif))|(img\/[^\.]+?.(jpg|jpeg|png|gif)\{[^\}]+?\})/);
+p a
+
+
 
 
