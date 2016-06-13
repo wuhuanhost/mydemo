615b1930caead2b5d029f6e05c000c3a9921b5fb
wuhuanhost <post.thinking@gmail.com>
2016-06-13 14:36:27 +0800

    update

---
+++ try-learn.js/demo/textflow/src/sizeutils.js
@@ -0,0 +1,20 @@
+/**
+ * 像素转毫米
+ * @return {[type]} [description]
+ */
+function px2mm(px) {
+    return px / 25.4;
+
+}
+
+
+/**
+ * 毫米转像素
+ * @param  {[type]} mm [description]
+ * @return {[type]}    [description]
+ */
+function mm2px(mm) {
+
+    return 25.4 * mm;
+
+}
